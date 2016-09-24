66161a86cba830a710f1e6a06f636ac2ff63760a
Melissa Martinez <melmar12@live.com>
2016-09-24 14:27:47 -0500

    bug fix

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,11 +2,18 @@ $(function () {
 
 	$(window).scroll(function () {
 
+		var is_mobile = false;
+		if ( $('.photo').css('justify-content') == 'center') {
+			is_mobile = true;
+		}
+
+		if (!is_mobile) {
 			var scrollin = $(window).scrollTop();
 			var offSet = (scrollin * -1.2);
 
 			if (scrollin < $(".text").height()) {
 				$(".photo").css({"margin-top": offSet.toString() + "px"});
 			}
+		}
 	});
 });
